3f5b04916236715d95f54cb2c9516e302806cc65
Nicholas de Waal <nick.a.dewaal@gmail.com>
2021-01-07 20:39:24 +0900

    stable

--- src/components/DrinkWater.js
+++ src/components/DrinkWater.js
@@ -1,7 +1,39 @@
 import React, { Fragment, useState } from 'react'
+import PropTypes from 'prop-types'
+
+const Cup = ({ percentage, setPercentage, remained, setRemained }) => {
+  const [isFull, setIsFull] = useState(false)
+
+  const handleCupClick = () => {
+    setIsFull(!isFull)
+
+    setPercentage(() => (isFull ? percentage - 12.5 : percentage + 12.5))
+
+    console.log('percentage', percentage)
+
+    setRemained(() => (isFull ? remained + 1.875 : remained - 1.875))
+  }
+
+  return (
+    <div
+      onClick={handleCupClick}
+      className={`${isFull ? 'cup cup-small full' : 'cup cup-small'}`}
+    >
+      250 ml
+    </div>
+  )
+}
+
+Cup.propTypes = {
+  percentage: PropTypes.number.isRequired,
+  remained: PropTypes.number.isRequired,
+  setPercentage: PropTypes.func.isRequired,
+  setRemained: PropTypes.func.isRequired,
+}
 
 const DrinkWater = () => {
   const [percentage, setPercentage] = useState(0)
+  const [remained, setRemained] = useState(15)
 
   return (
     <Fragment>
@@ -9,11 +41,25 @@ const DrinkWater = () => {
       <h3>Goal: 2 Liters</h3>
 
       <div className='cup'>
-        <div className='remained'>
-          <span id='liters'></span>
+        <div
+          className='remained'
+          style={{
+            visibility: `${remained === 0 ? 'hidden' : 'visible'}`,
+            height: `${remained === 0 && 0}`,
+          }}
+        >
+          <span id='liters'>{remained}L</span>
           <small>Remained</small>
         </div>
-        <div className='percentage'>{percentage}%</div>
+
+        {percentage !== 0 && (
+          <div
+            className='percentage'
+            style={{ height: `${3.3 * percentage}px` }}
+          >
+            {percentage}%
+          </div>
+        )}
       </div>
 
       <p className='text'>
@@ -21,14 +67,54 @@ const DrinkWater = () => {
       </p>
 
       <div className='cups'>
-        <div className='cup cup-small full'>250 ml</div>
-        <div className='cup cup-small full'>250 ml</div>
-        <div className='cup cup-small'>250 ml</div>
-        <div className='cup cup-small'>250 ml</div>
-        <div className='cup cup-small'>250 ml</div>
-        <div className='cup cup-small'>250 ml</div>
-        <div className='cup cup-small'>250 ml</div>
-        <div className='cup cup-small'>250 ml</div>
+        <Cup
+          percentage={percentage}
+          setPercentage={setPercentage}
+          remained={remained}
+          setRemained={setRemained}
+        />
+        <Cup
+          percentage={percentage}
+          setPercentage={setPercentage}
+          remained={remained}
+          setRemained={setRemained}
+        />
+        <Cup
+          percentage={percentage}
+          setPercentage={setPercentage}
+          remained={remained}
+          setRemained={setRemained}
+        />
+        <Cup
+          percentage={percentage}
+          setPercentage={setPercentage}
+          remained={remained}
+          setRemained={setRemained}
+        />
+        <Cup
+          percentage={percentage}
+          setPercentage={setPercentage}
+          remained={remained}
+          setRemained={setRemained}
+        />
+        <Cup
+          percentage={percentage}
+          setPercentage={setPercentage}
+          remained={remained}
+          setRemained={setRemained}
+        />
+        <Cup
+          percentage={percentage}
+          setPercentage={setPercentage}
+          remained={remained}
+          setRemained={setRemained}
+        />
+        <Cup
+          percentage={percentage}
+          setPercentage={setPercentage}
+          remained={remained}
+          setRemained={setRemained}
+        />
       </div>
     </Fragment>
   )
